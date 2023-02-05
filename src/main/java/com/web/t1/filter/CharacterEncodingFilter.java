1efab14b1a350d41179ef05ab5bf99c54d9b7bf1
jacktid <smsky1010@gmail.com>
2023-02-06 08:07:30 +0900

    csp

--- src/main/java/com/web/t1/filter/CharacterEncodingFilter.java
+++ src/main/java/com/web/t1/filter/CharacterEncodingFilter.java
@@ -8,6 +8,7 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
 
 @WebFilter("/*")
 public class CharacterEncodingFilter implements Filter {
@@ -18,8 +19,11 @@ public class CharacterEncodingFilter implements Filter {
 
 		System.out.println("before fileter");
 		arg0.setCharacterEncoding("UTF-8");
-	//	arg1.setHeader("Cache-Control","no-cache");
-		arg2.doFilter(arg0, arg1); // 넘기기 ㄴ
+		HttpServletResponse httpResponse = (HttpServletResponse) arg1;
+	    httpResponse.setHeader("Content-Security-Policy", "default-src 'self'");	
+		
+		//arg1.setHeader("Cache-Control","no-cache");
+		arg2.doFilter(arg0, arg1);
 		System.out.println("after fileter");
 		
 		
